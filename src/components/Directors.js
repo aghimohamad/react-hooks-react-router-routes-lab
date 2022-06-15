9f3c27e8d79871a7b0ad1b5820e66266fa5e23c5
mohamad <aghi.mohamad@gmail.com>
2022-06-15 12:40:18 +0300

    completed

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,17 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <>
+    <h1>Directors Page</h1>
+    {directors.map((director, index )=> {
+    return  <div key={index} >
+      {director.name}
+    <ul >{director.movies.map((movie, index) => <li key={index} > {movie} </li> )}</ul>
+    </div>
+    })}
+  </>
+  );
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,15 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
-}
 
+  return (<>
+    <h1>Movies Page</h1>
+    {movies.map((movie, index )=> {
+    return  <div key={index} >
+      {movie.title} - {movie.time}min
+    <ul >{movie.genres.map((genre, index) => <li key={index} > {genre} </li> )}</ul>
+    </div>
+    })}
+    </>)
+}
 export default Movies;
